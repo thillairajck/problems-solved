f8ad105e02bd31a7c7bfa33ecd40ab9a08621e78
ThillaiRaj <thillairajck08@gmail.com>
2026-09-08 12:15:55 +0530

    updated  project

--- dsa_class.java
+++ dsa_class.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class dsa_class {
     public static void main(String[] args) {
-        
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the String:");
+        String number=sc.next();
+        String r="";
+        int l=number.length();
+        StringBuilder sb=new StringBuilder(r);
+        for(int i=0;i<l-4;i++){
+            sb.append('*');
+        }
+        for(int i=l-4;i<l;i++){
+            sb.append(number.charAt(i));
+        }
+        System.out.println(sb.toString());
     }
 }
--- dsa_class1.java
+++ dsa_class1.java
@@ -1,6 +1,44 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class dsa_class1 {
     
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the first string:");
+        String a=sc.next();
+        System.out.print("Enter the second string:");
+        String b=sc.next();
+        Set<Character> set=new HashSet<>();
+        boolean f1=true;
+        boolean f2=true;
+        for(char c:b.toCharArray()){
+            boolean f=false;
+            for(char d:a.toCharArray()){
+                if(c==d){
+                    f=true;
+                    break;
+                }
+            }if(f){
+                set.add(c);
+            }
+            else{
+                f1=false;
+                break;
+            }
+        }
+        for(char c:a.toCharArray()){
+            if(!set.contains(c)){
+                f2=false;
+                break;
+            }
+        }
+        if(f1 && f2){
+            System.out.print("Rotation");
+        }
+        else{
+             System.out.print("Not Rotation");
+        }
     }
 }
